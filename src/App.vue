6d5841d2bf7b3a6e70c1b252d082fcbc01a8aa3f
Edson Sousa <edson3711.es@gmail.com>
2023-01-12 09:12:47 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    {{ nome }} {{ idade }}
+    <div v-text="nome"></div>
+    <input type="text" v-model="nome" placeholder="nome" />
+    <input type="text" v-model="idade" placeholder="idade" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      nome: "",
+      idade: "",
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+input {
+  background-color: rgb(225, 127, 255);
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  border-radius: 10px;
 }
 </style>
